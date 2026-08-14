362705a1010c19916ec8ff04ed5bbb55c0729366
Bandar197 <Bandar197@users.noreply.github.com>
2026-08-14 17:33:29 +0000

    Fix Betty style for hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
 
 /**
+ * update_value - Updates the value of an existing key
+ * @node: Node to update
+ * @value: New value
+ *
+ * Return: 1 on success, 0 on failure
+ */
+static int update_value(hash_node_t *node, const char *value)
+{
+	char *new_value;
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+	return (1);
+}
+
+/**
  * hash_table_set - Adds or updates an element in a hash table
  * @ht: Hash table
  * @key: Key
@@ -12,11 +32,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *node;
-	hash_node_t *new_node;
-	char *new_value;
 
-	if (ht == NULL || ht->array == NULL || key == NULL ||
-	    key[0] == '\0' || value == NULL || ht->size == 0)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
@@ -25,40 +42,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-
-			free(node->value);
-			node->value = new_value;
-			return (1);
-		}
-
+			return (update_value(node, value));
 		node = node->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (0);
 
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
 	{
-		free(new_node);
+		free(node->key);
+		free(node->value);
+		free(node);
 		return (0);
 	}
 
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
